abe06459fb529e154f3449afdd0a9054fa34d110
eborist <eborist@Ubuntu.myguest.virtualbox.org>
2024-12-07 15:03:55 +0100

    Added stuff

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 # Reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '83e5dfa'
+AESD_ASSIGNMENTS_VERSION = '73781cd2ded56e735978798f185f08a1bc636f93'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-istvanbor.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
